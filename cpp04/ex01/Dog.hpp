0283536b783caeab289e3871e21f530c682e9df2
scrumier <sonam.crumiere@icloud.com>
2024-09-18 16:50:55 +0200

    added ex01

---
+++ cpp04/ex01/Dog.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Dog.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: scrumier <scrumier@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/16 13:13:13 by scrumier          #+#    #+#             */
+/*   Updated: 2024/09/18 09:51:45 by scrumier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DOG_HPP
+# define DOG_HPP
+
+# include "Animal.hpp"
+
+class Dog : public Animal {
+	public:
+		Dog();
+		Dog(const Dog &src);
+		Dog &operator=(const Dog &src);
+		~Dog();
+
+		void makeSound() const;
+};
+
+#endif
---
+++ cpp04/ex01/main.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: scrumier <scrumier@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/16 13:10:17 by scrumier          #+#    #+#             */
+/*   Updated: 2024/09/18 09:49:20 by scrumier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Animal.hpp"
+#include "Dog.hpp"
+#include "Cat.hpp"
+
+int main( void )
+{
+	const Animal* meta = new Animal();
+	const Animal* j = new Dog();
+	const Animal* i = new Cat();
+
+	std::cout << "J Type: " << j->getType() << " " << std::endl;
+	std::cout << "I Type: " << i->getType() << " " << std::endl;
+	j->makeSound();
+	i->makeSound();
+	meta->makeSound();
+
+	delete  meta;
+	delete  j;
+	delete  i;
+
+	return 0;
+}
